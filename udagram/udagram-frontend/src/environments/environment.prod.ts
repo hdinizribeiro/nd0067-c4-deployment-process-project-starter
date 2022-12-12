4e235a6e02a2941a7209d3e2ad8dd28e45394c3f
Henrique Ribeiro <hdinizribeiro@gmail.com>
2022-12-12 08:38:12 -0300

    Fix environment files

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: process.env.URL || "http://localhost:8080/api/v0",
+  apiHost: "http://localhost:8080/api/v0",
 };
 
 /*
