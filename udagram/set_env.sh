d9789553570bdaeb417004be20e170b118dabb72
Henrique Ribeiro <hdinizribeiro@gmail.com>
2022-12-13 06:33:51 -0300

    Include script to deploy api also

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=postgres
+export POSTGRES_USERNAME=<postgres_username>
+export POSTGRES_PASSWORD=<postgres_password>
 export POSTGRES_HOST=localhost
 export POSTGRES_DB=udagramdb
-export AWS_BUCKET=arn:aws:s3:::hdinizudagram
-export AWS_REGION=us-east-2
+export AWS_BUCKET=<aws_bucket>
+export AWS_REGION=<aws_region>
 export AWS_PROFILE=default
-export JWT_SECRET=mysecretstring
+export JWT_SECRET=<jwt_secret>
 export URL=http://localhost:8100 
